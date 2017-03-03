6a2debfb8f4c525e8c3fea9d2fb7b60184efe907
Laura Moore <rileyle@gmail.com>
2017-03-03 17:53:53 -0500

    Added following and followable to UserSerializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,13 @@ class UsersController < ApplicationController
     render json: @users
   end
 
+  def show
+    render json: User.find(:id)
+  end
+
   def create
     @user = User.new(user_params)
-    if @user.save 
+    if @user.save
       render json: @user, serializer: UserExpandedSerializer
     else
       render json: @user.errors.full_messages, status: 401
