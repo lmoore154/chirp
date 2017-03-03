816109301951c36d503467029871dce7d09e775b
Laura Moore <rileyle@gmail.com>
2017-03-03 15:31:26 -0500

    Small changes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,15 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :email, :following, :followable
+  attributes :id, :first_name, :last_name, :username, :email#, :following, :followable
   has_many :posts
 
   private
-
-  def following
-    current_user.followees(User)
-  end
-
-  def followable
-    current_user.followers(User)
-  end
+  #
+  # def following
+  #   current_user.followees(User)
+  # end
+  #
+  # def followable
+  #   current_user.followers(User)
+  # end
 
 end
