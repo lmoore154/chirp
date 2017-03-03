528ec98f47923799f992eab79ff77eced1bcecb5
Laura Moore <rileyle@gmail.com>
2017-03-03 14:15:04 -0500

    Redirected root

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users
 
   post '/login' => 'sessions#create'
-  root 'users#create'
+  post '/signup' => 'users#create'
   get '/timeline' => 'posts#timeline'
   post '/follow/:id' => 'users#follow'
   post '/unfollow/:id' => 'users#unfollow'
